867e787b6c72847ec5a43a7a7356e1cbea646c5d
Aman Mishra <36539591+amanmishra1005@users.noreply.github.com>
2018-02-19 20:27:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res){
+    res.send('here article one is served');
+});
+
+
+app.get('/article_two',function(req,res){
+  res.send('here article two is served');
+});
+
+app.get('/article_three',function(req,res){
+    res.send('here request three is served');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
